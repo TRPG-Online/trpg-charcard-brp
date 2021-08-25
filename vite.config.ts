6ec370b092e9ab34f8abae6fd5f9ffdfaaa910e3
FairyScript <h@queb.fun>
2021-08-26 00:43:04 +0800

    add vite

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/out.ts'),
+      name: 'CharSheet',
+      fileName: format => `trpg-char.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'react-dom'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          ReactDOM: 'react-dom',
+        },
+      },
+    },
+  },
+})
